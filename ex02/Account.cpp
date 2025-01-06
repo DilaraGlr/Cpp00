00793521790d7756db80ca2f5b2d9e4e93bec5e1
Dilara Guler <dilaraguler@air-de-dilara.home>
2025-01-06 18:58:13 +0100

    ex02 finish

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,5 +1,7 @@
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
+#include <iomanip>
 
 // Initialisation des variables statiques
 int Account::_nbAccounts = 0;
@@ -39,37 +41,47 @@ Account::Account(int initial_deposit)
     : _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) {
     _nbAccounts++;          // Augmente le nombre total de comptes
     _totalAmount += initial_deposit; // Ajoute le montant du dépôt initial au total global
-    displayStatus();        // Affiche l'état du compte après initialisation
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";"
+				<< "amount:" << _amount << ";created" << std::endl;
 }
 
 // Destructeur
-Account::~Account(void) {
-    _nbAccounts--;          // Décrémente le nombre total de comptes
-    _totalAmount -= _amount; // Enlève le montant du compte du total global
+Account::~Account(void)
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
 }
 
 // Méthode pour faire un dépôt
 void Account::makeDeposit(int deposit) {
-    _amount += deposit;
-    _nbDeposits++;
+    _displayTimestamp();
+	this->_nbDeposits++;
+	std::cout << "index:" << _accountIndex << ";"
+				<< "p_amount:" << _amount << ";"
+				<< "deposit:" << deposit << ";amount:" << _amount
+					+ deposit << ";nb_deposits:" << this->_nbDeposits << std::endl;
+    this->_amount += deposit;
     _totalAmount += deposit;
     _totalNbDeposits++;
-    displayStatus();  // Affiche l'état du compte après dépôt
 }
 
 // Méthode pour faire un retrait
 bool Account::makeWithdrawal(int withdrawal) {
-    if (_amount < withdrawal) {
-        std::cout << "Withdrawal: Insufficient funds" << std::endl;
+    _displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";"
+				<< "p_amount:" << _amount << ";"
+				<< "withdrawal:" << withdrawal << ";";
+    if (checkAmount() < withdrawal) {
+        std::cout << "withdrawal:refused" << std::endl;
         return false; // Si le solde est insuffisant, l'opération échoue
-    } else {
+    }
         _amount -= withdrawal;
         _nbWithdrawals++;
         _totalAmount -= withdrawal;
         _totalNbWithdrawals++;
-        displayStatus(); // Affiche l'état du compte après retrait
+	    std::cout << withdrawal << ":amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
         return true;
-    }
 }
 
 // Méthode pour vérifier le montant du compte
@@ -80,12 +92,25 @@ int Account::checkAmount(void) const {
 // Méthode pour afficher l'état du compte
 void Account::displayStatus(void) const {
     _displayTimestamp();
-    std::cout << "account:" << _accountIndex << ";amount:" << _amount << ";deposits:" 
-              << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+    std::cout << "index:" << _accountIndex
+     << ";p_amount:" << _amount 
+     << ";deposits:" << _nbDeposits 
+     << ";withdrawals:" << _nbWithdrawals
+    << std::endl;
 }
 
 // Méthode privée pour afficher l'horodatage (timestamp)
 void Account::_displayTimestamp(void) {
     // Format de l'horodatage
-    std::cout << "[1992-01-04 14:54:06] "; // A adapter selon le format de l'exercice
+std::cout << std::setfill('0') << "[";
+	std::time_t now = std::time(0);
+	std::tm *local_time = std::localtime(&now);
+	std::cout << 1900 + local_time->tm_year;
+	std::cout << std::setw(2) << 1 + local_time->tm_mon;
+	std::cout << std::setw(2) << local_time->tm_mday;
+	std::cout << "_";
+	std::cout << std::setw(2) << local_time->tm_hour;
+	std::cout << std::setw(2) << local_time->tm_min;
+	std::cout << std::setw(2) << local_time->tm_sec;
+	std::cout << std::setw(2) << "] ";
 }
